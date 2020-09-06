4b54459c6a25a25b8ddee673159fe34d5484d561
Kpurek <kpurek@KpurekdeMacBook-Air.local>
2020-09-07 01:43:53 +0800

    commit view and test

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -1,5 +1,54 @@
 package test;
 
+import java.sql.*;
+
 public class TestMain {
-    
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/" +
+            "RUNOOB?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    static final String USER = "root";
+    static final String PASS = "123456";
+
+    public static void main(String[] args) {
+
+
+
+        Connection connection = null;
+        Statement statement = null;
+        try{
+            Class.forName(JDBC_DRIVER);
+
+            // 打开链接
+            System.out.println("连接数据库...");
+            connection = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            statement = connection.createStatement();
+            String sql;
+            sql = "SELECT id, name, url FROM websites";
+            ResultSet rs = statement.executeQuery(sql);
+
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                int id  = rs.getInt("id");
+                String name = rs.getString("name");
+                String url = rs.getString("url");
+
+                // 输出数据
+                System.out.print("ID: " + id);
+                System.out.print(", 站点名称: " + name);
+                System.out.print(", 站点 URL: " + url);
+                System.out.print("\n");
+            }
+            // 完成后关闭
+            rs.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ view/TestView.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class TestView extends JFrame {
+    public TestView(){
+        
+    }
+}
